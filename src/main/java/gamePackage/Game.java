b9dc25bae64ee2a5c4c41f2d3e9e8ccff7418d1c
Jean Gal <j.c.r.gal@student.tudelft.nl>
2019-11-25 16:50:22 +0100

    Managed to have the puck appear on the screen (Cas fixed it, unfortunately his source tree didn't work so I applied the change with my git account).

--- src/main/java/gamePackage/Game.java
+++ src/main/java/gamePackage/Game.java
@@ -7,21 +7,22 @@ public class Game extends JFrame{
     public static Puck puck;
 
     public static void main(String[] args) throws InterruptedException{
-        JFrame frame = new JFrame("Game Window");
+        JFrame frame = new GameFrame();
+        frame.setTitle("Game Window");
+        frame.setSize(500, 800);
+
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
 
         GameVector position = new GameVector((frame.getSize().getWidth()) / 2 + 1, (frame.getSize().getWidth() / 2) + 1);
         GameVector zeroVector = new GameVector(0, 0);
-        puck = new Puck(position, zeroVector, zeroVector);
 
+        puck = new Puck(position, zeroVector, zeroVector);
         panel.add(puck);
 
         frame.setContentPane(panel);
-
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 800);
         frame.setVisible(true);
 
         while(true){
@@ -30,12 +31,14 @@ public class Game extends JFrame{
         }
     }
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    static class GameFrame extends JFrame {
+        @Override
+        public void paint(Graphics g){
+            super.paint(g);
 
-        puck.paint(g);
-        g.fillOval(10, 10, 50, 50);
+            puck.paint(g);
+            g.fillOval((int) puck.position.getX(), (int) puck.position.getY(), 50, 50);
+        }
     }
 }
 
