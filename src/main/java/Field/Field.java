5e3664a60d35580fed44185a3d04e456444e9f03
Calvin Coule <c.b.coule@student.tudelft.nl>
2019-11-26 22:09:42 +0100

    Added documentation and added boxes for collision

--- src/main/java/Field/Field.java
+++ src/main/java/Field/Field.java
@@ -2,20 +2,36 @@ package Field;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-
+/**
+ * This class creates a field to play on.
+ */
 public class Field extends JPanel{
 
     private Image fieldImage;
+    private static Color myColor = new Color(0, 255,0, 127 );
+    private ArrayList<Rectangle> r = new ArrayList<Rectangle>();
 
-    public Field() {
+    /**
+     * Initiates the Drawing of a field.
+     * @param d the dimenions of a given field.
+     */
+    public Field(Dimension d) {
         createField();
+        createRectangle(d);
     }
 
+    /**
+     * Retrieves the image from the assets folder.
+     */
     public void getImage() {
         fieldImage = new ImageIcon("src/main/java/assets/airHockey.png").getImage();
     }
 
+    /**
+     * Sets the image and its preferred size.
+     */
     public void createField() {
         getImage();
         int w = fieldImage.getWidth(this);
@@ -23,9 +39,28 @@ public class Field extends JPanel{
         setPreferredSize(new Dimension(h, w));
     }
 
+    /**
+     * Creates the bounding boxes for collision.
+     * @param d the dimensions of the containing frame.
+     */
+    public void createRectangle(Dimension d) {
+        this.r.add(new Rectangle(0, 0, 11, d.width));
+        this.r.add(new Rectangle(d.width - 28, 0, d.height, 13));
+        this.r.add(new Rectangle(0, d.height - 51, 13, d.width));
+        this.r.add(new Rectangle(0, 0, d.height, 9));
+    }
+
+    /**
+     * Overrides the draw method and draws the desired objects.
+     * @param g the object that actually draws, given by the library.
+     */
     @Override
     public void paintComponent(Graphics g) {
         g.drawImage(fieldImage, 0, 0, null);
+        g.setColor(myColor);
+        for(int i = 0; i < r.size(); i++) {
+            g.fillRect(r.get(i).getX(), r.get(i).getY(), r.get(i).getWidth(), r.get(i).getHeight());
+        }
     }
 
 }
--- src/main/java/Field/Frame.java
+++ src/main/java/Field/Frame.java
@@ -2,18 +2,25 @@ package Field;
 
 import javax.swing.JFrame;
 
+/**
+ * This class creates the frame to draw everything in.
+ */
 public class Frame extends JFrame{
 
+    /**
+     * This method creates a new frame and initiates the necessary methods to draw everything.
+     */
     public Frame() {
-
         createFrame();
     }
 
+    /**
+     * This method creates the frame for the window and then draws everything in.
+     */
     private void createFrame() {
-
-        add(new Field());
-        setTitle("Board One");
         setSize(320, 640);
+        add(new Field(this.size()));
+        setTitle("Board One");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
