355cdfc6dc90a77fbd67c44630fb5ca86129d858
Calvin Coule <c.b.coule@student.tudelft.nl>
2019-11-28 09:51:25 +0000

    Update HelloWorld.java because of merge conflicts

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,12 +2,12 @@ import field.Frame;
 
 public class HelloWorld {
     public static void main(String[] args) {
-//        Student student = new Student("Iron Man", 2345678);
-//
-//        System.out.println("Hi there, " + student);
+        Student student = new Student("Iron Man", 2345678);
 
-            Frame f = new Frame();
-            f.setVisible(true);
-            f.setResizable(false);
+        System.out.println("Hi there, " + student);
+
+            // Frame f = new Frame();
+            // f.setVisible(true);
+            // f.setResizable(false);
     }
 }
