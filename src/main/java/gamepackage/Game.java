1623e2474ab36883606cbde3d730bb9457d9d53d
Calvin Coule <c.b.coule@student.tudelft.nl>
2019-11-28 13:18:16 +0100

    Added Oliver's changes (Login splash screen)

--- src/main/java/gamepackage/Game.java
+++ src/main/java/gamepackage/Game.java
@@ -2,7 +2,9 @@ package gamepackage;
 
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 import field.Frame;
 
@@ -12,6 +14,11 @@ public class Game extends JFrame {
     public static Puck puck;
     public static Frame frame;
     public static Board board;
+    public static JFrame loginScreen;
+    public static JTextField username;
+    public static JTextField password;
+    public static JButton button;
+    public static boolean login = false;
     private static final long serialVersionUID = 4714318125998709253L;
 
     /**
@@ -20,6 +27,12 @@ public class Game extends JFrame {
      * @throws InterruptedException Checks if thread has been interrupted.
      */
     public static void main(String[] args) throws InterruptedException {
+        //Creates a simple login screen
+        loginScreen();
+
+        //Checks if the play button is pressed, only then it can move on to the game screen
+        checkButton();
+
         frame = new Frame();
         frame.setVisible(true);
         frame.setResizable(false);
@@ -31,6 +44,49 @@ public class Game extends JFrame {
             puck.move(frame);
             Thread.sleep(10);
         }
+    }
+
+    /**
+     * Creates the loginScreen where the user has to fill in his credentials
+     */
+    public static void loginScreen(){
+        loginScreen = new JFrame();
+        loginScreen.setTitle("Login Window");
+        loginScreen.setSize(500, 800);
+        loginScreen.getContentPane().setLayout(new FlowLayout());
+
+        JLabel userName = new JLabel("Username : ");
+        JLabel passWord = new JLabel("Password : ");
+
+        username = new JTextField(30);
+        password = new JTextField(30);
+        button = new JButton("Start Game");
+
+        loginScreen.getContentPane().add(userName);
+        loginScreen.getContentPane().add(username);
+
+        loginScreen.getContentPane().add(passWord);
+        loginScreen.getContentPane().add(password);
+        loginScreen.getContentPane().add(button);
+        loginScreen.setVisible(true);
+    }
+
+
+
+
+    /**
+     * Keeps checking if the button is pressed, until it is pressed.
+     */
+    public static void checkButton(){
+        while(!login){
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    login = true;
+                }
+            });
+        }
+        loginScreen.setVisible(false);
     }
 }
 
