080a33c6aa4419706e1a2bfe95b9394e8663aa6a
Jean Gal <j.c.r.gal@student.tudelft.nl>
2019-12-02 19:05:39 +0100

    Created paddle class

---
+++ src/main/java/gamepackage/Paddle.java
@@ -0,0 +1,43 @@
+package gamepackage;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ * Class which defines a Paddle.
+ */
+public class Paddle extends JPanel {
+    private static final long serialVersionUID = 59692986L;
+
+    protected transient GameVector position;
+
+    /**
+     * Initializes a player paddle object for the game.
+     * @param position The starting position of the paddle
+     */
+    public Paddle(GameVector position) {
+        this.position = position;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.fillRect((int) this.position.getX(), (int) this.position.getY(), 75, 75);
+    }
+
+    /**
+     * Gets the position of the paddle.
+     * @return The position of the paddle
+     */
+    public GameVector getPosition() {
+        return this.position;
+    }
+
+    /**
+     * Sets the position of the paddle.
+     * @param position The new position of the paddle
+     */
+    public void setPosition(GameVector position) {
+        this.position = position;
+    }
+}
