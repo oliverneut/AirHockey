5b124f5d5a6c659b40f07135b31f8f5452eba90e
Oliver <o.l.c.neut@student.tudelft.nl>
2019-12-04 21:34:48 +0100

    Changed integration with GUI
    username and password are "user" and "user"

--- src/main/java/gamepackage/Game.java
+++ src/main/java/gamepackage/Game.java
@@ -13,12 +13,6 @@ public class Game extends JFrame {
 
     public static Puck puck;
     public static Frame frame;
-    public static Board board;
-    public static JFrame loginScreenT;
-    public static JTextField username;
-    public static JTextField password;
-    public static JButton button;
-    public static boolean login = false;
     private static final long serialVersionUID = 4714318125998709253L;
 
     /**
@@ -27,15 +21,9 @@ public class Game extends JFrame {
      * @throws InterruptedException Checks if thread has been interrupted.
      */
     public static void main(String[] args) throws InterruptedException {
-        //Creates a simple login screen
-        loginScreen();
 
-        //Checks if the play button is pressed, only then it can move on to the game screen
-        checkButton();
-
-        frame = new Frame();
-        frame.setVisible(true);
-        frame.setResizable(false);
+        GUI gui = new GUI();
+        gui.titleScreen();
 
         puck = frame.getPuck();
 
@@ -46,47 +34,6 @@ public class Game extends JFrame {
         }
     }
 
-    /**
-     * Creates the loginScreen where the user has to fill in his credentials
-     */
-    public static void loginScreen(){
-        loginScreenT = new JFrame();
-        loginScreenT.setTitle("Login Window");
-        loginScreenT.setSize(500, 800);
-        loginScreenT.getContentPane().setLayout(new FlowLayout());
 
-        JLabel userName = new JLabel("Username : ");
-        JLabel passWord = new JLabel("Password : ");
-
-        username = new JTextField(30);
-        password = new JTextField(30);
-        button = new JButton("Start Game");
-
-        loginScreenT.getContentPane().add(userName);
-        loginScreenT.getContentPane().add(username);
-
-        loginScreenT.getContentPane().add(passWord);
-        loginScreenT.getContentPane().add(password);
-        loginScreenT.getContentPane().add(button);
-        loginScreenT.setVisible(true);
-    }
-
-
-
-
-    /**
-     * Keeps checking if the button is pressed, until it is pressed.
-     */
-    public static void checkButton(){
-        while(!login){
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    login = true;
-                }
-            });
-        }
-        loginScreenT.setVisible(false);
-    }
 }
 
