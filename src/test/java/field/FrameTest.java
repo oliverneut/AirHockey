f15d6ddfe716063ee02e3aaa4bfb25e725c4af27
Calvin Coule <c.b.coule@student.tudelft.nl>
2019-12-05 10:25:28 +0100

    Uncommented the tests and added new ones where necessary

--- src/test/java/field/FrameTest.java
+++ src/test/java/field/FrameTest.java
@@ -1,31 +1,53 @@
 package field;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+
 public class FrameTest {
 
-//    static Frame frame = new Frame();
-//
-//    @Test
-//    void testSetter() {
-//        assertNotNull(frame);
-//    }
-//
-//    @Test
-//    void testGetPuck() {
-//        assertNotNull(frame.getPuck());
-//    }
-//
-//    @Test
-//    void testGetBoundingBoxes() {
-//        assertNotNull(frame.getBoundingBoxes());
-//    }
-//
-//    @Test
-//    void testgetGoals() {
-//        assertNotNull(frame.getGoals());
-//    }
+    static
+
+    @Test
+    void testSetter() {
+        try {
+            Frame frame = new Frame(1);
+            assertNotNull(frame);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    void testGetPuck() {
+        try {
+            Frame frame = new Frame(1);
+            assertNotNull(frame.getPucks());
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    void testGetBoundingBoxes() {
+        try {
+            Frame frame = new Frame(1);
+            assertNotNull(frame.getBoundingBoxes());
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    void testgetGoals() {        try {
+        Frame frame = new Frame(1);
+        assertNotNull(frame.getGoals());
+    } catch (FileNotFoundException e) {
+        System.out.println(e);
+    }
+
+    }
 }
--- src/test/java/gamepackage/MoveTest.java
+++ src/test/java/gamepackage/MoveTest.java
@@ -1,53 +1,53 @@
 package gamepackage;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class MoveTest {
-//
-//    private transient Puck puck;
-//
-//    private static double move = 10;
-//    private static double width = 500;
-//    private static double height = 800;
-//
-//    @BeforeEach
-//    void setupTestEnvironment() {
-//        //fix to test errors hopefully
-//        puck = new Puck(new GameVector((this.width) / 2 + 1,
-//                (this.height / 2) + 1), new GameVector(0, 0));
-//    }
-//
-//    @AfterEach
-//    void shutDownScreen() {
-//        //frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-//    }
-//
-//    @Test
-//    void testInitialize() {
-//        double calculatedX = this.width / 2 + 1;
-//        double calculatedY = this.height / 2 + 1;
-//        assertEquals(calculatedX, puck.getPosition().getX());
-//        assertEquals(calculatedY, puck.getPosition().getY());
-//    }
-//
-//    @Test
-//    void testVelocity() {
-//        puck.setVelocity(new GameVector(move, move));
-//        assertEquals(move, puck.getVelocity().getX());
-//        assertEquals(move, puck.getVelocity().getY());
-//    }
-//
-//    @Test
-//    void testMove() {
-//        double endX = puck.getPosition().getX() + move;
-//        double endY = puck.getPosition().getY() + move;
-//        puck.setVelocity(new GameVector(move, move));
-//        puck.move(null);
-//        assertEquals(endY, puck.getPosition().getY());
-//        assertEquals(endX, puck.getPosition().getX());
-//    }
+
+    private transient Puck puck;
+
+    private static double move = 10;
+    private static double width = 500;
+    private static double height = 800;
+
+    @BeforeEach
+    void setupTestEnvironment() {
+        //fix to test errors hopefully
+        puck = new Puck(new GameVector((this.width) / 2 + 1,
+                (this.height / 2) + 1), new GameVector(0, 0), 1, 50);
+    }
+
+    @AfterEach
+    void shutDownScreen() {
+        //frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+    }
+
+    @Test
+    void testInitialize() {
+        double calculatedX = this.width / 2 + 1;
+        double calculatedY = this.height / 2 + 1;
+        assertEquals(calculatedX, puck.getPosition().getX());
+        assertEquals(calculatedY, puck.getPosition().getY());
+    }
+
+    @Test
+    void testVelocity() {
+        puck.setVelocity(new GameVector(move, move));
+        assertEquals(move, puck.getVelocity().getX());
+        assertEquals(move, puck.getVelocity().getY());
+    }
+
+    @Test
+    void testMove() {
+        double endX = puck.getPosition().getX() + move;
+        double endY = puck.getPosition().getY() + move;
+        puck.setVelocity(new GameVector(move, move));
+        puck.move(null);
+        assertEquals(endY, puck.getPosition().getY());
+        assertEquals(endX, puck.getPosition().getX());
+    }
 }
