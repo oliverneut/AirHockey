2754b2230c3411538e37516e326f949ef5b6c0cb
Oliver <o.l.c.neut@student.tudelft.nl>
2020-01-12 15:46:16 +0100

    Functionality for the GUI to display friend leaderboard.
    Added the functionality for the user to see al his friends and their number of games played, games won and total of goals scored.

--- src/main/java/gui/FriendScreenController.java
+++ src/main/java/gui/FriendScreenController.java
@@ -1,5 +1,7 @@
 package gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +9,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 
@@ -15,6 +20,23 @@ import javafx.stage.Stage;
  */
 public class FriendScreenController {
     private transient Parent menuScreen = null;
+
+    @FXML
+    private TableView<PlayerRecord> friendsTable;
+
+    @FXML
+    private TableColumn<PlayerRecord, String> usernameColumn;
+
+    @FXML
+    private TableColumn<PlayerRecord, String> numGamesColumn;
+
+    @FXML
+    private TableColumn<PlayerRecord, String> gamesWonColumn;
+
+    @FXML
+    private TableColumn<PlayerRecord, String> goalsScoredColumn;
+
+
 
     /**
      * button to go back to previous screen.
@@ -40,5 +62,55 @@ public class FriendScreenController {
         Stage stage = (Stage) node.getScene().getWindow();
         stage.setScene(new Scene(menuScreen));
     }
+
+    @FXML
+    private void refreshTable(ActionEvent event){
+
+        usernameColumn = new TableColumn<>("Usename");
+        usernameColumn.setMinWidth(112);
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+
+        numGamesColumn = new TableColumn<>("#games");
+        numGamesColumn.setMinWidth(90);
+        numGamesColumn.setCellValueFactory(new PropertyValueFactory<>("numGames"));
+
+        gamesWonColumn = new TableColumn<>("#wins");
+        gamesWonColumn.setMinWidth(90);
+        gamesWonColumn.setCellValueFactory(new PropertyValueFactory<>("gamesWon"));
+
+        goalsScoredColumn = new TableColumn<>("#goals");
+        goalsScoredColumn.setMinWidth(90);
+        goalsScoredColumn.setCellValueFactory(new PropertyValueFactory<>("goalsScored"));
+
+        friendsTable.setItems(getPlayerRecord());
+
+        friendsTable.getColumns().addAll(usernameColumn, numGamesColumn, gamesWonColumn, goalsScoredColumn);
+
+    }
+
+
+    /**
+     * Gets all the data for each user which is a friend of the user that is playing.
+     * Data it retrieves : (games played, games won, goals scored).
+     * Then takes that data and creates a new PlayerRecord object for each friend.
+     * Adds all the PlayerRecord objects to an ObservableList and returns it.
+     * @return ObservableList with all the data of all friends.
+     */
+    public ObservableList<PlayerRecord> getPlayerRecord(){
+        ObservableList<PlayerRecord> playerRecords = FXCollections.observableArrayList();
+
+        //This is sample data , I need the DB data.
+        playerRecords.add(new PlayerRecord("Oliver", 5, 4, 7));
+        playerRecords.add(new PlayerRecord("Dixit", 10, 9, 100));
+        playerRecords.add(new PlayerRecord("Jean", 18, 3, 17));
+
+        return playerRecords;
+    }
+
+
+
+
+
+
 
 }
---
+++ src/main/java/gui/PlayerRecord.java
@@ -0,0 +1,50 @@
+package gui;
+
+public class PlayerRecord {
+
+
+    private String username;
+    private int numGames;
+    private int gamesWon;
+    private int goalsScored;
+
+    public PlayerRecord(String username, int numGames, int gamesWon, int goalsScored) {
+        this.username = username;
+        this.numGames = numGames;
+        this.gamesWon = gamesWon;
+        this.goalsScored = goalsScored;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getNumGames() {
+        return numGames;
+    }
+
+    public void setNumGames(int numGames) {
+        this.numGames = numGames;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public void setGamesWon(int gamesWon) {
+        this.gamesWon = gamesWon;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+}
