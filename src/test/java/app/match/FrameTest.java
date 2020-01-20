469f4080e9756e45e328aa3d1b0dfbab76d5dda7
Cas <C.J.Wever@student.tudelft.nl>
2020-01-20 12:16:47 +0100

    Add tests for match.frame

--- src/test/java/app/match/FrameTest.java
+++ src/test/java/app/match/FrameTest.java
@@ -1,4 +1,49 @@
 package app.match;
 
+import basis.GameVector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class FrameTest {
+
+    private static int height = 640;
+    private static int width = 320;
+
+    private transient Frame frame;
+
+
+    @Mock
+    private transient Match match;
+
+    @BeforeEach
+    void setUp() {
+        match = Mockito.mock(Match.class);
+        frame = new Frame(match, width, height,
+                new ArrayList(), new ArrayList());
+    }
+
+    @Test
+    void testCreatePaddleSelf() {
+        frame.createPaddle(height, width, false);
+        GameVector wantedPosition = new GameVector(width / 2, height / 4);
+        assertNotNull(frame.paddle);
+        assertEquals(wantedPosition, frame.paddle.getPosition());
+        assertEquals(new GameVector(0, 0), frame.paddle.getVelocity());
+    }
+
+    @Test
+    void testCreatePaddleOpponent() {
+        frame.createPaddle(height, width, true);
+        GameVector wantedPosition = new GameVector(width / 2, height / 4 * 3);
+        assertNotNull(frame.opponentPaddle);
+        assertEquals(wantedPosition, frame.opponentPaddle.getPosition());
+        assertEquals(new GameVector(0, 0), frame.opponentPaddle.getVelocity());
+    }
 }
