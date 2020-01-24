3e9444e2f158f0deb4053b154b07e196b00f98b0
Calvin Coule <c.b.coule@student.tudelft.nl>
2020-01-24 14:12:22 +0100

    Added requestUtil tests

---
+++ src/test/java/app/util/RequestUtilTest.java
@@ -0,0 +1,35 @@
+package app.util;
+
+import static app.util.RequestUtil.removeSessionAttrLoggedOut;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import spark.Request;
+import spark.Session;
+
+public class RequestUtilTest {
+
+    @Test
+    void testRemoveSessionAttrLoggedOutTrue() {
+        Request mockReq = Mockito.mock(Request.class);
+        Mockito.when(mockReq.session()).thenReturn(Mockito.mock(Session.class));
+        Mockito.when(mockReq.session().attribute("loggedOut"))
+            .thenReturn(Mockito.mock(Object.class));
+        assertTrue(removeSessionAttrLoggedOut(mockReq));
+    }
+
+    @Test
+    void testRemoveSessionAttrLoggedOutFalse() {
+        Request mockReq = Mockito.mock(Request.class);
+        Mockito.when(mockReq.session()).thenReturn(Mockito.mock(Session.class));
+        Mockito.when(mockReq.session().attribute("loggedOut"))
+            .thenReturn(null);
+        assertFalse(removeSessionAttrLoggedOut(mockReq));
+    }
+
+    @Test
+    void testSetterTest() {
+        assertNotNull(new RequestUtil());
+    }
+}
